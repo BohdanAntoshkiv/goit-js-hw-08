a198c16081416524ccefbfa776b15c2d8b0c223d
Bohdan Antoshkiv <Antoshkivbl@gmail.com>
2022-08-18 19:13:42 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const items = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>`
+    )
+    .join('');
+
+gallery.insertAdjacentHTML('afterbegin', items);
+
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
