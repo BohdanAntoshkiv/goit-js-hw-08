ae371452c43ec8c02a119d5814eecf44c9a81b70
Bohdan Antoshkiv <Antoshkivbl@gmail.com>
2022-08-21 10:42:42 +0300

    fitch task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,7 +3,7 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
 // player
 //   .getCurrentTime()
@@ -14,15 +14,24 @@ const LOCALSTORAGE_KEY = "videoplayer-current-time";
 //     // an error occurred
 //   });
 
-player.on('timeupdate', throttle(function getTime (event) {
+player.on(
+  'timeupdate',
+  throttle(function getTime(event) {
     const currentSecond = JSON.stringify(event.seconds);
-    
+
     localStorage.setItem(LOCALSTORAGE_KEY, currentSecond);
-}), 1000);
-
+  }),
+  1000
+);
+// Мій варіант який не працював----------------------------------------------------------------------------------
 // const timeFromLocalStor = Number(localStorage.getItem(LOCALSTORAGE_KEY));
-const timeFromLocalStor = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+// const timeFromLocalStor = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
-player
-    .setCurrentTime(timeFromLocalStor);
+// player.setCurrentTime(0 || timeFromLocalStor);
 
+const savedTime = localStorage.getItem(LOCALSTORAGE_KEY);
+if (savedTime) {
+  const timeFromLocalStor = JSON.parse(savedTime);
+
+  player.setCurrentTime(timeFromLocalStor);
+}
